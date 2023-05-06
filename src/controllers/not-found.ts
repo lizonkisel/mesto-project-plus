e7abd9b399607e5ce279c8ebf1e3ed9377e3abe4
lizonkisel <ees.hwork@gmail.com>
2023-05-06 19:17:44 +0300

    refactor: delete all not-found files

--- src/controllers/not-found.ts
+++
@@ -1,7 +0,0 @@
-import { Request, Response } from 'express';
-
-const getNotFoundPage = (req: Request, res: Response) => {
-  res.status(404).send({ message: 'Такой страницы не существует' });
-};
-
-export default getNotFoundPage;
--- src/routes/not-found.ts
+++
@@ -1,9 +0,0 @@
-import { Router } from 'express';
-
-import getNotFoundPage from '../controllers/not-found';
-
-const router = Router();
-
-router.all('/', getNotFoundPage);
-
-export default router;
