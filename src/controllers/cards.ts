f1def29e17c6df081ba545b457b8630e56259065
lizonkisel <ees.hwork@gmail.com>
2023-05-06 14:25:07 +0300

    refactor: rewrite cards controllers to try-catch and add CustomValid errors handlers

--- src/controllers/cards.ts
+++ src/controllers/cards.ts
@@ -1,151 +1,122 @@
 import { Request, Response } from 'express';
+import mongoose from 'mongoose';
 
 import { ERROR_CODE_400, ERROR_CODE_404, ERROR_CODE_500 } from '../utils/errors';
 import Card from '../models/card';
-import checkExistenceOfUser from '../utils/utils';
+import User from '../models/user';
 
-const getCards = (req: Request, res: Response) => {
-  Card.find({})
-    .populate('owner')
-    .then((cards) => res.status(200).send({ data: cards }))
-    .catch(() => res.status(ERROR_CODE_500).send({ message: 'Произошла какая-то ошибка' }));
+const getCards = async (req: Request, res: Response) => {
+  try {
+    const cards = await Card.find({}).populate('owner').populate('likes');
+    return res.status(200).send({ data: cards });
+  } catch (err) {
+    return res.status(ERROR_CODE_500).send({ message: 'Произошла какая-то ошибка' });
+  }
 };
 
 const postCard = async (req: Request, res: Response) => {
-  const { name, link } = req.body;
-
-  if (!name || !link) {
-    return res.status(ERROR_CODE_400).send({ message: 'Переданы некорректные данные в метод создания карточки' });
-  }
-
-  if (!req.body.user) {
-    return res.status(ERROR_CODE_400).send({ message: 'Не передан id автора' });
-  }
-
-  const id = req.body.user._id; // Временное решение, чтобы получить доступ к _id автора,
-  // который не передаётся в запросе
-
-  if (!id) {
-    return res.status(ERROR_CODE_400).send({ message: 'Не передан id автора' });
-  }
-
   try {
-    const owner = await checkExistenceOfUser(id);
-    if (owner === null) {
-      return res.status(ERROR_CODE_404).send({ message: 'Невозможно создать карточку за авторством несуществующего пользователя' });
+    const { name, link } = req.body;
+    if (!req.body.user || !req.body.user._id) {
+      const err = new Error('Не передан id автора');
+      err.name = 'CustomValid';
+      throw err;
     }
-  } catch (err: any) {
-    if (err.name === 'CastError') {
+    const author = await User.findById(req.body.user._id).orFail();
+    const card = await Card.create({ name, link, owner: req.body.user._id });
+    return res.status(201).send({ data: card });
+  } catch (err) {
+    if (err instanceof Error && err.name === 'CustomValid') {
+      return res.status(400).send({ message: err.message });
+    }
+    if (err instanceof mongoose.Error.DocumentNotFoundError) {
+      return res.status(ERROR_CODE_404).send({ message: 'Автор не найден' });
+    }
+    if (err instanceof mongoose.Error.CastError) {
       return res.status(ERROR_CODE_400).send({ message: 'Передан невалидный id автора' });
+    }
+    if (err instanceof mongoose.Error.ValidationError) {
+      return res.status(ERROR_CODE_400).send({ message: 'Переданы некорректные данные в метод создания карточки' });
     }
     return res.status(ERROR_CODE_500).send({ message: 'На сервере произошла ошибка' });
   }
-
-  return Card.create({ name, link, owner: id })
-    .then((card) => res.status(201).send({ data: card }))
-    .catch(() => res.status(500).send({ message: 'Произошла какая-то ошибка' }));
 };
 
-const deleteCard = (req: Request, res: Response) => {
-  if (!req.params.cardId) {
-    return res.status(ERROR_CODE_400).send({ message: 'Не передан id карточки' });
+const deleteCard = async (req: Request, res: Response) => {
+  try {
+    const card = await Card.findByIdAndRemove(req.params.cardId).orFail();
+    return res.status(200).send({ data: card });
+  } catch (err) {
+    if (err instanceof mongoose.Error.DocumentNotFoundError) {
+      return res.status(ERROR_CODE_404).send({ message: 'Карточка не найдена' });
+    }
+    if (err instanceof mongoose.Error.CastError) {
+      return res.status(ERROR_CODE_400).send({ message: 'Передан невалидный id карточки' });
+    }
+    return res.status(ERROR_CODE_500).send({ message: 'На сервере произошла ошибка' });
   }
-
-  return Card.findByIdAndRemove(req.params.cardId)
-    .then((card) => {
-      if (card === null) {
-        return res.status(ERROR_CODE_404).send({ message: 'Карточка не найдена' });
-      }
-      return res.status(200).send({ data: card });
-    })
-    .catch((err) => {
-      if (err.name === 'CastError') {
-        return res.status(ERROR_CODE_400).send({ message: 'Передан невалидный id карточки' });
-      }
-      return res.status(ERROR_CODE_500).send({ message: 'На сервере произошла ошибка' });
-    });
 };
 
 const putLike = async (req: Request, res: Response) => {
-  if (!req.params.cardId) {
-    return res.status(ERROR_CODE_400).send({ message: 'Не передан id карточки' });
-  }
-
-  if (!req.body.user || !req.body.user._id) {
-    return res.status(ERROR_CODE_400).send({ message: 'Не передан id пользователя' });
-  }
-
   try {
-    const owner = await checkExistenceOfUser(req.body.user._id);
-    if (owner === null) {
-      return res.status(ERROR_CODE_404).send({ message: 'Невозможно поставить лайк за авторством несуществующего пользователя' });
+    if (!req.body.user || !req.body.user._id) {
+      const err = new Error('Не передан id автора');
+      err.name = 'CustomValid';
+      throw err;
     }
-  } catch (err: any) {
-    if (err.name === 'CastError') {
-      return res.status(ERROR_CODE_400).send({ message: 'Передан невалидный id автора' });
+    const author = await User.findById(req.body.user._id).orFail();
+    const card = await Card.findByIdAndUpdate(
+      req.params.cardId,
+      { $addToSet: { likes: req.body.user._id } },
+      { new: true },
+    )
+      .populate('owner')
+      .populate('likes')
+      .orFail();
+    return res.status(200).send({ data: card });
+  } catch (err) {
+    if (err instanceof Error && err.name === 'CustomValid') {
+      return res.status(400).send({ message: err.message });
+    }
+    if (err instanceof mongoose.Error.DocumentNotFoundError) {
+      return res.status(ERROR_CODE_404).send({ message: 'Карточка или автор не найдены' });
+    }
+    if (err instanceof mongoose.Error.CastError) {
+      return res.status(ERROR_CODE_400).send({ message: 'Передан невалидный id карточки или автора' });
     }
     return res.status(ERROR_CODE_500).send({ message: 'На сервере произошла ошибка' });
   }
-
-  return Card.findByIdAndUpdate(
-    req.params.cardId,
-    { $addToSet: { likes: req.body.user._id } },
-    { new: true },
-  )
-    .populate('likes')
-    .then((card) => {
-      if (card === null) {
-        return res.status(ERROR_CODE_404).send({ message: 'Карточка не найдена' });
-      }
-      return res.status(200).send({ data: card });
-    })
-    .catch((err) => {
-      if (err.name === 'CastError') {
-        return res.status(ERROR_CODE_400).send({ message: 'Передан невалидный id карточки' });
-      }
-      return res.status(ERROR_CODE_500).send({ message: 'На сервере произошла ошибка' });
-    });
 };
 
 const deleteLike = async (req: Request, res: Response) => {
-  if (!req.params.cardId) {
-    return res.status(ERROR_CODE_400).send({ message: 'Не передан id карточки' });
-  }
-
-  if (!req.body.user || !req.body.user._id) {
-    return res.status(ERROR_CODE_400).send({ message: 'Не передан id пользователя' });
-  }
-
   try {
-    const owner = await checkExistenceOfUser(req.body.user._id);
-    if (owner === null) {
-      return res.status(ERROR_CODE_404).send({ message: 'Невозможно удалить лайк за авторством несуществующего пользователя' });
+    if (!req.body.user || !req.body.user._id) {
+      const err = new Error('Не передан id автора');
+      err.name = 'CustomValid';
+      throw err;
     }
-  } catch (err: any) {
-    if (err.name === 'CastError') {
-      return res.status(ERROR_CODE_400).send({ message: 'Передан невалидный id автора' });
+    const author = await User.findById(req.body.user._id).orFail();
+    const card = await Card.findByIdAndUpdate(
+      req.params.cardId,
+      { $pull: { likes: req.body.user._id } },
+      { new: true },
+    )
+      .populate('owner')
+      .populate('likes')
+      .orFail();
+    return res.status(200).send({ data: card });
+  } catch (err) {
+    if (err instanceof Error && err.name === 'CustomValid') {
+      return res.status(400).send({ message: err.message });
+    }
+    if (err instanceof mongoose.Error.DocumentNotFoundError) {
+      return res.status(ERROR_CODE_404).send({ message: 'Карточка или автор не найдены' });
+    }
+    if (err instanceof mongoose.Error.CastError) {
+      return res.status(ERROR_CODE_400).send({ message: 'Передан невалидный id карточки или автора' });
     }
     return res.status(ERROR_CODE_500).send({ message: 'На сервере произошла ошибка' });
   }
-
-  return Card.findByIdAndUpdate(
-    req.params.cardId,
-    { $pull: { likes: req.body.user._id } },
-    { new: true },
-  )
-    .populate('likes')
-    .then((card) => {
-      if (card === null) {
-        return res.status(ERROR_CODE_404).send({ message: 'Карточка не найдена' });
-      }
-      return res.status(200).send({ data: card });
-    })
-    .catch((err) => {
-      if (err.name === 'CastError') {
-        return res.status(ERROR_CODE_400).send({ message: 'Передан невалидный id карточки' });
-      }
-      return res.status(ERROR_CODE_500).send({ message: 'На сервере произошла ошибка' });
-    });
 };
 
 export {
--- src/utils/utils.ts
+++
@@ -1,8 +0,0 @@
-import User from '../models/user';
-
-const checkExistenceOfUser = async (id: string) => {
-  const user = await User.findById(id);
-  return user;
-};
-
-export default checkExistenceOfUser;
